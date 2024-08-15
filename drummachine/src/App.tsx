8140198a61eaece957f151f72c78605ef5911bef
dcurtin91 <109159085+dcurtin91@users.noreply.github.com>
2024-08-14 19:18:51 -0500

    Update App.tsx

--- drummachine/src/App.tsx
+++ drummachine/src/App.tsx
@@ -1,18 +1,27 @@
-import { useState } from 'react'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+
+
+  const play = (e: React.MouseEvent<HTMLDivElement>) => {
+   
+    const audio = e.currentTarget.querySelector("audio");
+    if (audio) {
+      audio.play();
+    }
+  }
 
   return (
     <>
       <div id="drum-machine">
         
-      <div id="display">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-       
+      <div id="display"></div>
+
+      <div className="drum-pad" id="heater1" onClick={play}>
+      <audio src="https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-1.mp3"></audio>
+        Q
       </div>
       </div>
     </>
