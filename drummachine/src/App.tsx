a04b0fe7108a1cf12bcf26e924dd03d1c889d983
dcurtin91 <109159085+dcurtin91@users.noreply.github.com>
2024-08-15 07:46:19 -0500

    Update App.tsx

--- drummachine/src/App.tsx
+++ drummachine/src/App.tsx
@@ -23,7 +23,40 @@ function App() {
       <audio src="https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-1.mp3"></audio>
         Q
       </div>
+      <div className="drum-pad" id="heater2" onClick={play}>
+      <audio src="https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-2.mp3"></audio>
+        W
       </div>
+      <div className="drum-pad" id="heater3" onClick={play}>
+      <audio src="https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-3.mp3"></audio>
+        E
+      </div>
+      <div className="drum-pad" id="heater4" onClick={play}>
+      <audio src="https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-4_1.mp3"></audio>
+        A
+      </div>
+      <div className="drum-pad" id="clap" onClick={play}>
+      <audio src="https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-6.mp3"></audio>
+        S
+      </div>
+      <div className="drum-pad" id="openhh" onClick={play}>
+      <audio src="https://cdn.freecodecamp.org/testable-projects-fcc/audio/Dsc_Oh.mp3"></audio>
+        D
+      </div>
+      <div className="drum-pad" id="kickhat" onClick={play}>
+      <audio src="https://cdn.freecodecamp.org/testable-projects-fcc/audio/Kick_n_Hat.mp3"></audio>
+        Z
+      </div>
+      <div className="drum-pad" id="kick" onClick={play}>
+      <audio src="https://cdn.freecodecamp.org/testable-projects-fcc/audio/RP4_KICK_1.mp3"></audio>
+        X
+      </div>
+      <div className="drum-pad" id="closedhh" onClick={play}>
+      <audio src="https://cdn.freecodecamp.org/testable-projects-fcc/audio/Cev_H2.mp3"></audio>
+        C
+      </div>
+      </div>
+      
     </>
   )
 }
